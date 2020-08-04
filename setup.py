c1d529db550e059f0215bf63e4b410c428f1f0a8
zhenyachet <zhenyachet91@gmail.com>
2020-08-04 14:37:53 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ProjectNews',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='evzen',
+    author_email='zhenyachet91@gmail.com',
+    description='News aggregator'
+)
